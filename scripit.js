e65702bf9007f0de556e2d9d65a0665819d7b207
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-01-30 21:41:21 -0300

    Adcionado atributos para correção automatica e ajustes finos

--- scripit.js
+++ scripit.js
@@ -10,7 +10,7 @@ let nomeCliente
 let enderecoCliente
 let mensagem_wpp;
 
-
+// Função que verifica se três itens estão selecionados, para o botão "Fechar Pedido" tornar-se clicável
 function validaBotao() {
     if (prato_selecionado != undefined && bebida_selecionado != undefined && sobremesa_selecionado != undefined) {
         document.getElementById("botao-finalizar").style.backgroundColor = "#32B72F"
@@ -32,12 +32,11 @@ function selecionarFrango() {
     //Deselecionar outro item
     document.getElementById("XDriven").style.boxShadow = "inset 0 0 0 5px #FFFFFF";
     document.getElementById("XDriven-select").style.display = "none";
-    // Armazenar item selecionado
+    // Armazenar informações item selecionado
     prato_selecionado = "Frango Yin Yang";
     pratoPreco = 14.90;
     // Função que valida se os tres itens foram selecionados
     validaBotao();
-    // Deselecionar
 
 }
 
@@ -51,9 +50,10 @@ function selecionarVegano() {
     //Deselecionar outro item
     document.getElementById("XDriven").style.boxShadow = "inset 0 0 0 5px #FFFFFF";
     document.getElementById("XDriven-select").style.display = "none";
-    // Armazenar item selecionado
+    // Armazenar informações item selecionado
     prato_selecionado = "Frango Vegano";
     pratoPreco = 15.90;
+    // Função que valida se os tres itens foram selecionados
     validaBotao();
 
 }
@@ -68,9 +68,10 @@ function selecionarXDriven() {
     //Deselecionar outro item
     document.getElementById("Vegano").style.boxShadow = "inset 0 0 0 5px #FFFFFF";
     document.getElementById("Vegano-select").style.display = "none";
-    // Armazenar item selecionado
+    // Armazenar informações item selecionado
     prato_selecionado = "XDriven";
     pratoPreco = 14.90;
+    // Função que valida se os tres itens foram selecionados
     validaBotao();
 }
 
@@ -85,9 +86,10 @@ function selecionarCoca() {
     //Deselecionar outro item
     document.getElementById("Cha").style.boxShadow = "inset 0 0 0 5px #FFFFFF";
     document.getElementById("Cha-select").style.display = "none";
-    // Armazenar item selecionado
+    // Armazenar informações item selecionado
     bebida_selecionado = "Coquinha gelada";
     bebidaPreco = 4.90;
+    // Função que valida se os tres itens foram selecionados
     validaBotao();
 
 }
@@ -102,9 +104,10 @@ function selecionarSuco() {
     //Deselecionar outro item
     document.getElementById("Cha").style.boxShadow = "inset 0 0 0 5px #FFFFFF";
     document.getElementById("Cha-select").style.display = "none";
-    // Armazenar item selecionado
+    // Armazenar informações item selecionado
     bebida_selecionado = "Suco de pera";
     bebidaPreco = 5.90;
+    // Função que valida se os tres itens foram selecionados
     validaBotao();
 
 }
@@ -119,9 +122,10 @@ function selecionarCha() {
     //Deselecionar outro item
     document.getElementById("Coca").style.boxShadow = "inset 0 0 0 5px #FFFFFF";
     document.getElementById("Coca-select").style.display = "none";
-    // Armazenar item selecionado
+    // Armazenar informações item selecionado
     bebida_selecionado = "Chá Matte";
     bebidaPreco = 9.90;
+    // Função que valida se os tres itens foram selecionados
     validaBotao();
 
 }
@@ -137,9 +141,10 @@ function selecionarPudim() {
     //Deselecionar outro item
     document.getElementById("SaladaFrutas").style.boxShadow = "inset 0 0 0 5px #FFFFFF";
     document.getElementById("SaladaFrutas-select").style.display = "none";
-    // Armazenar item selecionado
+    // Armazenar informações item selecionado
     sobremesa_selecionado = "Pudim";
     sobremesaPreco = 7.90;
+    // Função que valida se os tres itens foram selecionados
     validaBotao();
 
 }
@@ -154,9 +159,10 @@ function selecionarSorvete() {
     //Deselecionar outro item
     document.getElementById("SaladaFrutas").style.boxShadow = "inset 0 0 0 5px #FFFFFF";
     document.getElementById("SaladaFrutas-select").style.display = "none";
-    // Armazenar item selecionado
+    // Armazenar informações item selecionado
     sobremesa_selecionado = "Sorvete vegano"
     sobremesaPreco = 10.90;
+    // Função que valida se os tres itens foram selecionados
     validaBotao();
 }
 
@@ -170,10 +176,10 @@ function selecionarSaladaFrutas() {
     //Deselecionar outro item
     document.getElementById("Pudim").style.boxShadow = "inset 0 0 0 5px #FFFFFF";
     document.getElementById("Pudim-select").style.display = "none";
-    // Armazenar item selecionado
+    // Armazenar informações item selecionado
     sobremesa_selecionado = "Salada de frutas"
-    // Validando botão
     sobremesaPreco = 5.99;
+    // Função que valida se os tres itens foram selecionados
     validaBotao();
 }
 // Finalizar pedido
@@ -183,11 +189,11 @@ function revisarDados() {
     document.getElementById("bebida-nome").textContent = bebida_selecionado;
     document.getElementById("sobremesa-nome").textContent = sobremesa_selecionado;
     // Preços
-    document.getElementById("prato-preco").textContent = pratoPreco.toFixed(2);
-    document.getElementById("bebida-preco").textContent = bebidaPreco.toFixed(2);
-    document.getElementById("sobremesa-preco").textContent = sobremesaPreco.toFixed(2);
+    document.getElementById("prato-preco").textContent = pratoPreco.toFixed(2).toString().replace(".", ",");
+    document.getElementById("bebida-preco").textContent = bebidaPreco.toFixed(2).toString().replace(".", ",");
+    document.getElementById("sobremesa-preco").textContent = sobremesaPreco.toFixed(2).toString().replace(".", ",");
     // Total
-    document.getElementById("preco-total").textContent = "R$ " + precoTotal;
+    document.getElementById("preco-total").textContent = "R$ " + precoTotal.toString().replace(".", ",");
     // Revisar pedido
     document.getElementById("revisaoDados").style.display = "flex"
 
@@ -197,8 +203,8 @@ function finalizarPedido() {
     enderecoCliente = prompt("Por favor digite o seu Endereço: ")
     mensagem_wpp = "Olá, gostaria de fazer o pedido:" + "\n" + " - Prato: " + prato_selecionado + "\n" + " - Bebida: " + bebida_selecionado + "\n" + " - Sobremesa: " + sobremesa_selecionado + "\n" + "Total: R$ " + precoTotal + "\n \n" + "Nome: " + nomeCliente + "\n" + "Enderço: " + enderecoCliente;
     window.open("https://wa.me/5561999999999?text=" + encodeURIComponent(mensagem_wpp))
+    location.reload();
 }
 function cancelarPedido() {
     document.getElementById("revisaoDados").style.display = "none";
 }
-
